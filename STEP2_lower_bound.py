830d72fcc089ef2a2beb33712a5fdfa448a4bb52
mmakoto623 <46061108+mmakoto623@users.noreply.github.com>
2021-09-12 21:51:06 +0900

    Create STEP2_lower_bound.py

---
+++ STEP2_lower_bound.py
@@ -0,0 +1,30 @@
+#二分探索メニュー lower_bound
+##    A_i >= k を満たす最小の i を返す
+##    A_i >= k を満たす i が存在しない場合は n を返す
+def binary_search(a,n, k):
+#a : 数列, n : 数列のサイズ, k : 基準
+    #// 探索範囲 [left, right]
+    left = 0
+    right = n
+    
+    #// 探索範囲を狭めていく
+    while left < right:
+        #// 探索範囲の中央
+        mid = (left + right) // 2
+
+        if a[mid] < k:
+            #// a[0] ~ a[mid] は k 未満なので調べる必要が無い
+            left = mid+1
+        else:
+            right = mid
+    
+    return right
+
+n = int(input())
+A = [int(i) for i in input().split()]
+A.sort()
+
+q = int(input())
+for i in range(q):
+    k = int(input())
+    print(len(A)-binary_search(A,len(A),k))
