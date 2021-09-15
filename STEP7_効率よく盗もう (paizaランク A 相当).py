88992f8cb6c5f1eea3086371b07c0db026b5bb75
mmakoto623 <46061108+mmakoto623@users.noreply.github.com>
2021-09-15 23:51:34 +0900

    Create STEP7_効率よく盗もう (paizaランク A 相当).py

---
+++ STEP7_効率よく盗もう (paizaランク A 相当).py
@@ -0,0 +1,26 @@
+#STEP7_効率よく盗もう (paizaランク A 相当).py
+
+import decimal
+
+# 有効数字10桁指定
+decimal.getcontext().prec = 10
+
+n,k = map(int,input().split())
+W = [int(i) for i in input().split()]
+V = [int(i) for i in input().split()]
+
+left=0.0
+right=5001.0
+for i in range(50):
+    mid = (left+right)/2
+    tmp=[]
+    for i in range(n):
+        tmp.append(V[i]-(W[i]*mid))
+    tmp = sorted(tmp , reverse = True)
+    if sum(tmp[:k]) >= 0:
+        left = mid
+    else:
+        right = mid
+
+print(left)
+
